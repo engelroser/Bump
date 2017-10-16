1501a651e1fd1be02d44f2bbfc4cb706bb038b95
mobiel dev <engel.roser99999@gmail.com>
2017-10-16 04:57:04 +0300

    MISearchResponse

---
+++ Mixably/Classes/Model/SearchResponse.swift
@@ -0,0 +1,21 @@
+//
+//  MISearchResponse.swift
+//  Mixably
+//
+//  Created by Mobile App Developer on 25/02/17.
+//  Copyright © 2017 Mixably. All rights reserved.
+//
+
+import Foundation
+
+/**
+ MISearchResponse keeps serach responce
+ */
+
+class MISearchResponse{
+    
+    var tracks = [MITrack]()
+    var albums = [MIAlbum]()
+    var artist = [MIArtist]()
+
+}
